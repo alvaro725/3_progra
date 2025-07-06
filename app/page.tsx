8c1f6b1a71bb623682e0b78913de5e12addeca41
alvaro725 <alvaro.olivares16@inacapmail.cl>
2025-07-06 17:45:26 -0400

    page_arreglo

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [persona, setPersona] = useState({});
+  const [persona, setPersona] = useState({ nombre: "", apellido: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPersona({ ...persona, [name]: value });
+  };
 
   return (
     <form>
-      <input name="nombre" />
-      <input name="apellido" />
+      <input name="nombre" value={persona.nombre} onChange={handleChange} />
+      <input name="apellido" value={persona.apellido} onChange={handleChange} />
       <button>Guardar</button>
     </form>
   );
